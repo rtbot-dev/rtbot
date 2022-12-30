4266361fa3495477167c2b355bdec99208bbee66
Yuriel <yurielnf@gmail.com>
2022-12-30 18:45:15 +0100

    style(core): format with clang format

--- core/src/Operator.h
+++ core/src/Operator.h
@@ -1,91 +1,76 @@
 
-#include <vector>
 #include <string>
+#include <vector>
 
-namespace rtbot
-{
-    using std::string;
-    using std::vector;
+namespace rtbot {
+using std::string;
+using std::vector;
 
-    /// @brief Base class
-    /// @tparam T 
-    template <class T>
-    class Buffer
-    {
-        vector<T> data;
-        int pos = 0;
+/// @brief Base class
+/// @tparam T
+template <class T> class Buffer {
+  vector<T> data;
+  int pos = 0;
 
-    public:
-        int channelSize;
-        int windowSize;
+public:
+  int channelSize;
+  int windowSize;
 
-        Buffer(int channelSize_, int windowSize_)
-            : data(channelSize_ * windowSize_, T(0)), channelSize(channelSize_), windowSize(windowSize_)
-        {
-        }
+  Buffer(int channelSize_, int windowSize_)
+      : data(channelSize_ * windowSize_, T(0)), channelSize(channelSize_),
+        windowSize(windowSize_) {}
 
-        /// @brief element at channel i and time stamp j
-        T &operator(int i, int j) { return data.at(i + j * channelSize); }
-        const T &operator(int i, int j) const { return data.at(i + j * channelSize); }
+  /// @brief element at channel i and time stamp j
+  T &operator(int i, int j) { return data.at(i + j * channelSize); }
+  const T &operator(int i, int j) const { return data.at(i + j * channelSize); }
 
-        /// @brief The message msg at the last
-        /// @param values
-        void add(const vector<T> &msg)
-        {
-            if (msg.size() != channelSize)
-                throw std::__throw_invalid_argument("Buffer::add() with wrong number of channels");
-            for (int i = 0; i < channelSize; i++)
-                (*this)(i, pos) = msg[i];
-            pos = (pos + 1) % windowSize;
-        }
+  /// @brief The message msg at the last
+  /// @param values
+  void add(const vector<T> &msg) {
+    if (msg.size() != channelSize)
+      throw std::__throw_invalid_argument(
+          "Buffer::add() with wrong number of channels");
+    for (int i = 0; i < channelSize; i++)
+      (*this)(i, pos) = msg[i];
+    pos = (pos + 1) % windowSize;
+  }
 
-    private:
-    };
+private:
+};
 
-    template <class T>
-    class Op
-    {
-        vector<Op> children;
+template <class T> class Op {
+  vector<Op> children;
 
-    public:
-        const string id;
+public:
+  const string id;
 
-        Op(string const &id_) : id(id_) {}
+  Op(string const &id_) : id(id_) {}
 
-        virtual void receive(int t, Buffer const &msg) = 0;
+  virtual void receive(int t, Buffer const &msg) = 0;
 
-        void emit(int t, Buffer const &msg) const
-        {
-            for (const Op &x : children)
-                x.receive(t, msg);
-        }
+  void emit(int t, Buffer const &msg) const {
+    for (const Op &x : children)
+      x.receive(t, msg);
+  }
 
-        void addChildren(Op const &child)
-        {
-            children.push_back(child);
-        }
-    };
+  void addChildren(Op const &child) { children.push_back(child); }
+};
 
-    class MA : public Op<double>
-    {
+class MA : public Op<double> {
 
-    public:
-        struct Param
-        {
-            int backward = 0;
-            int forward = 0;
-        };
+public:
+  struct Param {
+    int backward = 0;
+    int forward = 0;
+  };
 
-        MA(string id_, Param const &pars_ = {})
-            : Op<double>(id_), pars(pars_)
-        {}
+  MA(string id_, Param const &pars_ = {}) : Op<double>(id_), pars(pars_) {}
 
-    private:
-        Param pars;
-    };
+private:
+  Param pars;
+};
 
-
-    template class Buffer<double>;
-    template class Op<double>;
+template class Buffer<double>;
+template class Op<double>;
 
 } // end namespace rtbot
--- core/src/rtbot.cpp
+++ core/src/rtbot.cpp
@@ -1,15 +1,15 @@
-#include <ctime>
-#include <string>
-#include <iostream>
 #include "yaml-cpp/yaml.h"
+#include <ctime>
+#include <iostream>
+#include <string>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
-  for (std::size_t i=0;i<primes.size();i++) {
+  for (std::size_t i = 0; i < primes.size(); i++) {
     std::cout << primes[i].as<int>() << "\n";
   }
 
-  for(const auto& x:primes)
+  for (const auto &x : primes)
     std::cout << x << "\n";
 
   return 0;
