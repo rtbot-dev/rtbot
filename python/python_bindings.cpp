61ad1c00cd412d6444c7cf83c0bd7f0206730684
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-01-03 08:31:07 +0000

    style(python): reformat python bindings with clang format

--- python/python_bindings.cpp
+++ python/python_bindings.cpp
@@ -1,10 +1,10 @@
 
-#include <pybind11/numpy.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/functional.h>
-#include <pybind11/stl.h>
 #include <pybind11/cast.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
 
 #include "rtbot/Buffer.h"
 
@@ -14,19 +14,14 @@ using namespace rtbot;
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-
 PYBIND11_MODULE(rtbotpy, m) {
-    m.doc() = "Python interface for real time bot library (rtbot)";
-    
+  m.doc() = "Python interface for real time bot library (rtbot)";
 
-    using Bufferd=Buffer<double>;
-    py::class_<Bufferd>(m,"Buffer")
-            .def(py::init<int,int>(), "channelSize"_a=1, "windowSize"_a=1)
-            .def_readonly("channelSize",&Bufferd::channelSize)
-            .def_readonly("windowSize",&Bufferd::windowSize)
-            .def("add",&Bufferd::add)
-            .def("getData",&Bufferd::getData)
-            ;
-
+  using Bufferd = Buffer<double>;
+  py::class_<Bufferd>(m, "Buffer")
+      .def(py::init<int, int>(), "channelSize"_a = 1, "windowSize"_a = 1)
+      .def_readonly("channelSize", &Bufferd::channelSize)
+      .def_readonly("windowSize", &Bufferd::windowSize)
+      .def("add", &Bufferd::add)
+      .def("getData", &Bufferd::getData);
 }
-
