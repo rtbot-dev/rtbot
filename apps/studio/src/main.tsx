f0b202829d60d9a93075b30c67124251f4e18f32
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-01-15 15:46:07 +0000

    feat(studio): add rtbot initial logo

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -1,39 +1,8 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Error } from "@/pages/Error";
-import { Profile } from "@/pages/Profile";
-import { SignUp } from "@/pages/SignUp";
-import { Workspace } from "@/pages/Workspace";
-import { SignIn } from "./pages/SignIn";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SignIn />,
-  },
-  {
-    path: "/signin",
-    element: <SignIn />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/error",
-    element: <Error />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/workspace",
-    element: <Workspace />,
-  },
-]);
+import { router } from "./routes";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
