33d6a1ef51ac7ed150572973d2ae9bcd5c2769fe
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-01-15 16:07:23 +0000

    build(studio): configure and deploy using firebase

---
+++ apps/studio/src/api/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqB0uKOBnMh85SgneXjLid9aOsCQmDnqU",
+  authDomain: "rtbot-6515d.firebaseapp.com",
+  projectId: "rtbot-6515d",
+  storageBucket: "rtbot-6515d.appspot.com",
+  messagingSenderId: "454936526147",
+  appId: "1:454936526147:web:fda411d0b1ba72b35743f6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
