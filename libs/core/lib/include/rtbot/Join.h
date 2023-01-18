0bc226fb38854192cfa87c008a2c7fcf9ee122ee
Yuriel <yurielnf@gmail.com>
2023-01-18 17:34:58 +0100

    refactor(core): add comments and reorganize class Join

--- libs/core/lib/include/rtbot/Join.h
+++ libs/core/lib/include/rtbot/Join.h
@@ -19,35 +19,49 @@ public:
 
     void receive(Message<T> const &msg, const Operator<T> *sender) override
     {
-        if (data.empty())
+        if (data.empty())   // prepare the data when the first message arrives
             data.resize(this->parents.size());
-        int channel = this->parents.at(sender); // will throw if not found
+
+        // add the incoming message to the correct channel
+        int channel = this->parents.at(sender);
         data[channel].push(msg);
 
-        bool all_ready = true;
-        for (auto &qi : data) {
+        // remove old messages
+        for (auto &qi : data)
             while (!qi.empty() && qi.front().time < msg.time)
                 qi.pop();
+
+        // check if all queue match the current time
+        bool all_ready = true;
+        for (const auto &qi : data)
             if (qi.empty() || qi.front().time > msg.time)
                 all_ready = false;
-        }
-        if (all_ready) {
-            Message<T> bigMsg;
-            bigMsg.time = msg.time;
-            for (const auto &qi : data)
-                for (const T &x : qi.front().value)
-                    bigMsg.value.push_back(x);
-            for (auto &qi : data)
-                qi.pop();
-            processData(bigMsg);
-        }
+
+        if (all_ready)
+            processData(makeMessage());
     }
+
 
     /**
      *  This is a replacement of Operator::receive but using the already synchronized data provided in msg
      *  It is responsible to emit().
      */
     virtual void processData(Message<T> const &msg) { this->emit(msg); };
+
+private:
+    // build a message by concatenating all channels front() data. Remove the used data.
+    Message<T> makeMessage()
+    {
+        Message<T> msg;
+        msg.time = data.at(0).front().time;
+        for (const auto &qi : data)
+            for (const T &x : qi.front().value)
+                msg.value.push_back(x);
+
+        for (auto &qi : data)
+            qi.pop();
+        return msg;
+    }
 };
 
 } // end namespace rtbot
