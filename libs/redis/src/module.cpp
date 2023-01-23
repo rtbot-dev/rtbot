6ca7ed85b99ec5fce576ba9878165c0db78fc039
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-01-23 19:49:05 +0000

    feat(redis): check if key is of the right type on run command

--- libs/redis/src/module.cpp
+++ libs/redis/src/module.cpp
@@ -2,7 +2,19 @@
 
 #include <stdlib.h>
 
-int RtBotRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+#include <iostream>
+
+int RtBotRun_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ);
+
+  int keytype = RedisModule_KeyType(key);
+  RedisModule_Log(ctx, "warning", "key type=%i", keytype);
+
+  if (keytype != REDISMODULE_KEYTYPE_MODULE) {
+    RedisModule_CloseKey(key);
+    return RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
+  }
+
   RedisModule_ReplyWithLongLong(ctx, rand());
   return REDISMODULE_OK;
 }
@@ -14,7 +26,7 @@ extern "C" {
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   if (RedisModule_Init(ctx, "rtbot", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) return REDISMODULE_ERR;
 
-  if (RedisModule_CreateCommand(ctx, "rtbot.rand", RtBotRand_RedisCommand, "fast random", 0, 0, 0) == REDISMODULE_ERR)
+  if (RedisModule_CreateCommand(ctx, "rtbot.run", RtBotRun_RedisCommand, "fast random", 0, 0, 0) == REDISMODULE_ERR)
     return REDISMODULE_ERR;
 
   return REDISMODULE_OK;
