7698c285b634422f9f78b69809742ed0351e02e2
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-01-23 16:06:43 +0000

    fix(redis): prevent mangling of redis module entry function while compiling

--- libs/redis/include/module.h
+++ libs/redis/include/module.h
@@ -2,6 +2,4 @@
 #define MODULE_H
 #include "redismodule.h"
 
-int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
-
 #endif
--- libs/redis/src/module.cpp
+++ libs/redis/src/module.cpp
@@ -7,6 +7,10 @@ int RtBotRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
   return REDISMODULE_OK;
 }
 
+// we need the `extern "C"` here to prevent symbol name mangling
+// it is likely that there is another way of achieving the same
+// with certain compiler options
+extern "C" {
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   if (RedisModule_Init(ctx, "rtbot", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) return REDISMODULE_ERR;
 
@@ -14,4 +18,5 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     return REDISMODULE_ERR;
 
   return REDISMODULE_OK;
+}
 }
