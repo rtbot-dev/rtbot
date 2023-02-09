a3f3daa60f7f83ea989ca3ba4ec5eca9fb6709d3
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-02-09 18:56:58 +0000

    feat(redis): publish pubsub message on pipeline events

--- libs/redis-module/src/commands/rtbot_xrun.rs
+++ libs/redis-module/src/commands/rtbot_xrun.rs
@@ -1,30 +1,36 @@
 use crate::cxx_bindings::ffi::RtBotMessage;
 use crate::pipelines_registry::PIPELINES_REGISTRY;
+use redis_module::RedisError::Str;
 use redis_module::{Context, NotifyEvent, RedisError, RedisResult, RedisString, RedisValue};
 use std::collections::BTreeMap;
 
-pub fn on_generic_event(ctx: &Context, _event_type: NotifyEvent, _event: &str, _key: &str) {
+pub fn on_generic_event(
+    ctx: &Context,
+    _event_type: NotifyEvent,
+    _event: &str,
+    _key: &str,
+) -> RedisResult {
     if _event == "ts.add" {
-        let last = match ctx.call("ts.get", &[_key]).unwrap() {
-            RedisValue::Array(arr) => arr,
-            _ => {
-                ctx.log_warning("Response from 'ts.get' is not an array");
-                return;
-            }
+        let last = if let RedisValue::Array(arr) = ctx.call("ts.get", &[_key]).unwrap() {
+            arr
+        } else {
+            return Err(Str("Response from 'ts.get' is not an array"));
         };
 
-        let timestamp = match &last[0] {
-            RedisValue::Integer(v) => Some(v),
-            _ => None,
-        }
-        .unwrap();
-        let value = match &last[1] {
-            RedisValue::SimpleString(s) => Some(s),
-            _ => None,
-        }
-        .unwrap()
-        .parse::<f64>()
-        .unwrap();
+        let timestamp = if let RedisValue::Integer(ts) = &last[0] {
+            ts
+        } else {
+            return Err(Str(
+                "Response from 'ts.get' first element is not a valid timestamp",
+            ));
+        };
+        let value = if let RedisValue::SimpleString(s) = &last[1] {
+            s.parse::<f64>().unwrap()
+        } else {
+            return Err(Str(
+                "Response from 'ts.get' first element is not a valid 64 bit float",
+            ));
+        };
         // and send it to the pipelines registered for the key
         let binding = PIPELINES_REGISTRY.read().unwrap();
         let registry = binding.as_ref().unwrap();
@@ -34,48 +40,42 @@ pub fn on_generic_event(ctx: &Context, _event_type: NotifyEvent, _event: &str, _
         // now for each of the registered outputs send the result
         for (registered_output_key, message) in result {
             if !message.is_empty() {
-                // println!("Sending message {:?} to {}", message, registered_output_key);
                 // notice that here we just consider the first element in the values as
                 // the value to consider as the output to store in the output timeseries
                 let timestamp = message[0].timestamp;
                 let value = message[0].values[0];
 
-                match ctx.call(
+                ctx.call(
                     "ts.add",
                     &[
                         registered_output_key.as_str(),
                         timestamp.to_string().as_str(),
                         value.to_string().as_str(),
                     ],
-                ) {
-                    Ok(_) => {}
-                    Err(_) => {
-                        ctx.log_warning(
-                            format!("Unable to send message to {}", registered_output_key).as_str(),
-                        );
-                        return;
-                    }
-                };
+                )?;
+                // send the saved message to the correspondent output pubsub channel
+                ctx.call(
+                    "publish",
+                    &[
+                        // channel
+                        format!("{}:ps", registered_output_key).as_str(),
+                        // value: a comma separated string where the first element is the timestamp
+                        // and the remaining ones the values
+                        format!("{},{}", message[0].timestamp, message[0].values[0]).as_str(),
+                    ],
+                )?;
             }
         }
     }
+    Ok(RedisValue::from(()))
 }
 
 pub fn run<'a>(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
     let mut it = args.into_iter();
     it.next(); // pop the first argument, which is the command itself
-    let program_key = it
-        .next()
-        .ok_or(RedisError::Str("No program key provided"))?
-        .to_string();
-    let input_key = it
-        .next()
-        .ok_or(RedisError::Str("No input key provided"))?
-        .to_string();
-    let output_key = it
-        .next()
-        .ok_or(RedisError::Str("No output key provided"))?
-        .to_string();
+    let program_key = it.next().ok_or(Str("No program key provided"))?.to_string();
+    let input_key = it.next().ok_or(Str("No input key provided"))?.to_string();
+    let output_key = it.next().ok_or(Str("No output key provided"))?.to_string();
 
     // run the usual program, but requiring a persistent pipeline
     ctx.call(
