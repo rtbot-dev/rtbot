8746aef2147c26b5efb1297a14cc77ff66849952
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-04-26 10:44:03 +0100

    fix(core): messages from children operators not being gathered

--- web/apps/studio/src/api/rtbot/rtbot.worker.ts
+++ web/apps/studio/src/api/rtbot/rtbot.worker.ts
@@ -35,7 +35,7 @@ export class RtBotRun {
       ...op.parameters,
       type: op.opType,
       // TODO: remove this patch when we complete the implementation of the Input and resamplers
-      ...(op.opType === "Input" ? { iType: "cosine", dt: 8 } : {}),
+      ...(op.opType === "Input" ? { iType: "cosine", dt: 7 } : {}),
     }));
     const connections = this.program.connections.filter((c) => c.from !== null && c.to !== null);
     return JSON.stringify({ ...this.program, operators, connections });
@@ -52,7 +52,7 @@ export class RtBotRun {
     // iterate over the data passed and send it to the rtbot program
     this.data.forEach(([time, ...value]) => {
       const iterationOutput = rtbot.receiveMessageInPipelineDebug(pipelineId, time, value);
-      //console.log("iteration output", iterationOutput);
+      //console.log("iteration ", time, value, "=>", iterationOutput);
       // record the outputs
       Object.entries(JSON.parse(iterationOutput) as RtBotIterationOutput).forEach(([k, msgs]) => {
         msgs.forEach(({ time, value }) => {
