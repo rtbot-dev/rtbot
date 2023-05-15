916a34110bdd19eb60d97ebb3d22a254cce1abd4
Yuriel <yurielnf@gmail.com>
2023-05-15 12:13:57 +0200

    wrapper(python): plot pipeline on python

--- libs/core/wrappers/python/rtbot.cpp
+++ libs/core/wrappers/python/rtbot.cpp
@@ -7,6 +7,7 @@
 #include <pybind11/stl.h>
 
 #include "rtbot/std/Matching.h"
+#include "rtbot/Pipeline.h"
 
 using namespace std;
 using namespace rtbot;
@@ -17,16 +18,27 @@ using namespace pybind11::literals;
 template<class T>
 void defineInModule(py::module& m)
 {
-  using Matchingd = Matching<T>;
-  py::class_<Matchingd>(m, "Matching")
+  py::class_<Matching<T>>(m, "Matching")
       .def(py::init<vector<T>, vector<T>, function<bool(T,T)>>(), "A"_a, "B"_a, "match_condition"_a)
       .def(py::init<vector<T>, vector<T>>(), "A"_a, "B"_a)
-      .def_readonly("matchA", &Matchingd::matchA)
-      .def_readonly("matchB", &Matchingd::matchB)
-      .def_readonly("extraA", &Matchingd::extraA)
-      .def_readonly("extraB", &Matchingd::extraB)
-      .def("fScore", &Matchingd::fScore)
-      .def("averageDistanceOfMatched", &Matchingd::averageDistanceOfMatched)
+      .def_readonly("matchA", &Matching<T>::matchA)
+      .def_readonly("matchB", &Matching<T>::matchB)
+      .def_readonly("extraA", &Matching<T>::extraA)
+      .def_readonly("extraB", &Matching<T>::extraB)
+      .def("fScore", &Matching<T>::fScore)
+      .def("averageDistanceOfMatched", &Matching<T>::averageDistanceOfMatched)
+      ;
+
+  using Msg=Message<std::uint64_t,double>;
+  py::class_<Msg>(m,"Message")
+      .def(py::init<std::int64_t,double>(),"time"_a=0, "value"_a=0)
+      .def_readwrite("time",&Msg::time)
+      .def_readwrite("value",&Msg::value)
+      ;
+
+  py::class_<Pipeline>(m,"Pipeline")
+      .def(py::init<string>(),"json_prog"_a)
+      .def("receiveDebug", &Pipeline::receiveDebug);
       ;
 }
 
