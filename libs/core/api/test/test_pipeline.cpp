a3989e7f6530eca8c0d9a0526be07fd8f13f0501
Eduardo Perez Verdecia <eduarduseldorf@gmail.com>
2023-05-29 20:43:25 -0700

    chore: additional test to validate multiple ports supporting code

--- libs/core/api/test/test_pipeline.cpp
+++ libs/core/api/test/test_pipeline.cpp
@@ -13,8 +13,8 @@ using namespace std;
 TEST_CASE("read ppg pipeline") {
   nlohmann::json json;
   {
-    ifstream in("examples/data/ppg.json");
-    if (!in) throw runtime_error("file ppg.json not found");
+    ifstream in("examples/data/ppg-test-1.json");
+    if (!in) throw runtime_error("file not found");
     in >> json;
   }
 
@@ -36,6 +36,37 @@ TEST_CASE("read ppg pipeline") {
     for (auto i = 0u; i < s.ti.size(); i++) {
       auto y = receiveMessageInPipeline("pipe1", Message<std::uint64_t, double>(s.ti[i], s.ppg[i]))[0];
       if (y) cout << y.value() << endl;
+    }
+  }
+}
+
+TEST_CASE("read  pipeline test data basic data") {
+  nlohmann::json json;
+  {
+    ifstream in("examples/data/ppg-test-2.json");
+    if (!in) throw runtime_error("file not found");
+    in >> json;
+  }
+
+  SECTION("using the pipeline") {
+    auto pipe = FactoryOp::createPipeline(json.dump().c_str());
+
+    // process the data
+    for (int i = 0; i < 100; i++) {
+      auto output = pipe.receiveDebug(Message<std::uint64_t, double>(i, i % 5));
+
+      if (i > 5 && i % 5 == 1) {
+        REQUIRE(output["out1"].size() == 1);
+        REQUIRE(output["out1"].at(0).value == 4);
+        REQUIRE(output["out1"].at(0).time == i - 2);
+
+        REQUIRE(output["out2"].size() == 1);
+        REQUIRE(output["out2"].at(0).value == 4);
+        REQUIRE(output["out2"].at(0).time == i - 2);
+      } else {
+        REQUIRE(output["out1"].size() == 0);
+        REQUIRE(output["out2"].size() == 0);
+      }
     }
   }
 }
