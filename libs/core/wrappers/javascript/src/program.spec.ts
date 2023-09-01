cfea3e7c7a6f53e7285d6d2a50fd5e683bdbf088
Robert Carcassés Quevedo <carcasses@gmail.com>
2023-09-01 16:09:10 +0100

    build: fix issues after merge

--- libs/core/wrappers/javascript/src/program.spec.ts
+++ libs/core/wrappers/javascript/src/program.spec.ts
@@ -1,4 +1,4 @@
-import { Program, MovingAverage, Input, Output, RtBotRun } from "@rtbot-dev/rtbot";
+import { Program, MovingAverage, Input, Output, RtBotRun, RtBotRunOutputFormat } from "@rtbot-dev/rtbot";
 
 const data = [
   [1, 10.0],
@@ -148,9 +148,11 @@ describe("Program", () => {
   });
 
   it("can process data", async () => {
-    const rtbotRun = new RtBotRun(program, data);
-    const result = await rtbotRun.run();
-    console.log("result", result);
+    const rtbotRun = new RtBotRun(program, data, RtBotRunOutputFormat.EXTENDED);
+    await rtbotRun.run();
+    const result = rtbotRun.getOutputs();
+    expect(result).toBeDefined();
+    expect(result.length).toBe(data.length);
   });
 
   it("there has to be at least one Input operator in a program to be valid", () => {
