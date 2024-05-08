2c8bee268bae49fd5cd63f4319b33e774d018bd2
Eduardo Perez Verdecia <eduarduseldorf@gmail.com>
2024-05-08 14:49:44 -0700

    fix(output): Fixing output filter implementation

---
+++ libs/api/test/test_bollinger_bands_generated.cpp
@@ -0,0 +1,70 @@
+#include <catch2/catch.hpp>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "rtbot/FactoryOp.h"
+#include "rtbot/bindings.h"
+#include "tools.h"
+
+using namespace rtbot;
+using namespace std;
+
+TEST_CASE("read  pipeline test bollinger_bands") {
+  nlohmann::json json;
+  {
+    ifstream in("examples/data/program-test-6.json");
+    if (!in) throw runtime_error("file not found");
+    in >> json;
+  }
+
+  SECTION("test generated bollinger_bands") {
+    auto pipe = FactoryOp::createProgram(json.dump().c_str());
+
+    srand(time(nullptr));
+    vector<int> v;
+    int start = 0;
+    int end = 10000;
+    int o1 = 0;
+    int o2 = 0;
+    int lastdown = -1;
+    int lastup = -1;
+    v.push_back(start);
+    for (int i = start + 1; i < end; i++) {
+      int random_value = rand();
+      if (random_value < (RAND_MAX / 2)) {
+        v.push_back(i);
+      }
+    }
+    v.push_back(end);
+
+    for (int i = 0; i < v.size(); i++) {
+      map<string, vector<Message<uint64_t, double>>> messagesMap;
+      vector<Message<uint64_t, double>> x;
+      x.push_back(Message<uint64_t, double>(v.at(i), v.at(i) * v.at(i)));
+      messagesMap.emplace("i1", x);
+
+      auto output = pipe.receive(messagesMap);
+
+      if (output.count("37") > 0) {
+        if (output.find("37")->second.count("o1") > 0) {
+          for (int j = 0; j < output.find("37")->second.find("o1")->second.size(); j++) {
+            int now = output.find("37")->second.find("o1")->second.at(j).time;
+            if (lastup >= 0) REQUIRE(now - 1 == lastup);
+            lastup = now;
+            o1++;
+          }
+        }
+        if (output.find("37")->second.count("o2") > 0) {
+          for (int j = 0; j < output.find("37")->second.find("o2")->second.size(); j++) {
+            int now = output.find("37")->second.find("o2")->second.at(j).time;
+            if (lastdown >= 0) REQUIRE(now - 1 == lastdown);
+            lastdown = now;
+            o2++;
+          }
+        }
+      }
+    }
+    REQUIRE(o1 == o2);
+  }
+}
---
+++ libs/std/test/test_cosine_resampler.cpp
@@ -0,0 +1,73 @@
+#include <catch2/catch.hpp>
+
+#include "rtbot/std/CosineResampler.h"
+
+using namespace rtbot;
+using namespace std;
+
+TEST_CASE("Cosine Resampler test emit at right frequencies") {
+  auto i1 = CosineResampler<uint64_t, double>("i1", 99);
+  auto i2 = CosineResampler<uint64_t, double>("i2", 99);
+  auto i3 = CosineResampler<uint64_t, double>("i3", 1);
+
+  SECTION("emits once") {
+    for (int i = 0; i < 20; i++) {
+      i1.receiveData(Message<uint64_t, double>(i * 100, i * i));
+      map<string, map<string, vector<Message<uint64_t, double>>>> emitted = i1.executeData();
+
+      if (i == 0)
+        REQUIRE(emitted.empty());
+      else {
+        REQUIRE(emitted.find("i1")->second.find("o1")->second.at(0).time == 99 * i);
+      }
+    }
+  }
+
+  SECTION("emits twice") {
+    for (int i = 0; i < 20; i++) {
+      i2.receiveData(Message<uint64_t, double>(i * 200, i * i));
+      map<string, map<string, vector<Message<uint64_t, double>>>> emitted = i2.executeData();
+
+      if (i == 0)
+        REQUIRE(emitted.empty());
+      else {
+        REQUIRE(emitted.find("i2")->second.find("o1")->second.at(0).time == 99 * (2 * i - 1));
+        REQUIRE(emitted.find("i2")->second.find("o1")->second.at(1).time == 99 * (2 * i));
+      }
+    }
+  }
+
+  SECTION("emits every time, one next to the next one") {
+    srand(time(nullptr));
+    vector<int> v;
+    int start = 0;
+    int end = 2000;
+    int last = start;
+    v.push_back(start);
+    for (int i = start + 1; i < end; i++) {
+      int random_value = rand();
+      if (random_value < (RAND_MAX / 2)) {
+        v.push_back(i);
+      }
+    }
+    v.push_back(end);
+
+    for (int i = 0; i < v.size(); i++) {
+      i3.receiveData(Message<uint64_t, double>(v.at(i), v.at(i) * v.at(i)));
+      map<string, map<string, vector<Message<uint64_t, double>>>> emitted = i3.executeData();
+
+      if (i == 0) {
+        // cout << "******* (" << v.at(i) << ") *******" << endl;
+        REQUIRE(emitted.empty());
+      } else {
+        // cout << "******* (" << v.at(i) << ") *******" << endl;
+        for (int j = 0; j < emitted.find("i3")->second.find("o1")->second.size(); j++) {
+          int now = emitted.find("i3")->second.find("o1")->second.at(j).time;
+          REQUIRE(now - 1 == last);
+          last = now;
+          // cout << now << endl;
+        }
+      }
+    }
+  }
+}
