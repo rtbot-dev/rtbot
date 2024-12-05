fd2dab24d84813a250c234eb10f0be9aa64e41f2
Robert Carcassés Quevedo <carcasses@gmail.com>
2024-12-05 21:02:36 +0000

    fix(apps): rewrite benchmark using the new api

--- apps/benchmark/src/benchmark.cpp
+++ apps/benchmark/src/benchmark.cpp
@@ -1,13 +1,16 @@
 #include <benchmark/benchmark.h>
 
+#ifdef __APPLE__
+#define BENCHMARK_OS_MAC
+#endif
+
 #include <cmath>
 #include <iostream>
 
-#include "rtbot/Collector.h"
 #include "rtbot/Input.h"
 #include "rtbot/Join.h"
 #include "rtbot/Output.h"
-#include "rtbot/std/Minus.h"
+#include "rtbot/std/MathSyncBinaryOp.h"
 #include "rtbot/std/MovingAverage.h"
 #include "rtbot/std/PeakDetector.h"
 #include "tools.h"
@@ -15,54 +18,50 @@
 using namespace rtbot;
 
 class RtBotFixture : public benchmark::Fixture {
-  public:
-    std::unique_ptr<Input<uint64_t, double>> i1;
-    std::unique_ptr<SamplePPG> s;
-    std::unique_ptr<MovingAverage<uint64_t, double>> ma1;
-    std::unique_ptr<MovingAverage<uint64_t, double>> ma2;
-    std::unique_ptr<Minus<uint64_t, double>> diff;
-    std::unique_ptr<PeakDetector<uint64_t, double>> peak;
-    std::unique_ptr<Join<uint64_t, double>> join;
-    std::unique_ptr<Output<uint64_t, double>> o1;
+ public:
+  std::shared_ptr<Input> i1;
+  std::unique_ptr<SamplePPG> s;
+  std::shared_ptr<MovingAverage> ma1;
+  std::shared_ptr<MovingAverage> ma2;
+  std::shared_ptr<Subtraction> diff;
+  std::shared_ptr<PeakDetector> peak;
+  std::shared_ptr<Join> join;
+  std::shared_ptr<Output> o1;
 
   void SetUp(const ::benchmark::State& state) {
-    if (i1.get() == nullptr) {
+    if (!i1) {
       s = std::make_unique<SamplePPG>("examples/data/ppg.csv");
-      i1 = std::make_unique<Input<uint64_t, double>>("i1");
-      ma1 = std::make_unique<MovingAverage<uint64_t, double>>("ma1", round(50 / s->dt()));
-      ma2 = std::make_unique<MovingAverage<uint64_t, double>>("ma2", round(2000 / s->dt()));
-      diff = std::make_unique<Minus<uint64_t, double>>("diff");
-      peak = std::make_unique<PeakDetector<uint64_t, double>>("peak", 2 * ma1->getDataInputMaxSize() + 1);
-      join = std::make_unique<Join<uint64_t, double>>("join", 2);
-      o1 = std::make_unique<Output<uint64_t, double>>("o1");
+      i1 = std::make_shared<Input>("i1", std::vector<std::string>{PortType::NUMBER});
+      ma1 = std::make_shared<MovingAverage>("ma1", round(50 / s->dt()));
+      ma2 = std::make_shared<MovingAverage>("ma2", round(2000 / s->dt()));
+      diff = std::make_shared<Subtraction>("diff");
+      peak = std::make_shared<PeakDetector>("peak", 2 * round(50 / s->dt()) + 1);
+      join = std::make_shared<Join>("join", std::vector<std::string>{PortType::NUMBER, PortType::NUMBER});
+      o1 = std::make_shared<Output>("o1", std::vector<std::string>{PortType::NUMBER});
 
-      // draw the pipeline
-      i1->connect(*ma1)
-          ->connect(*diff, "o1", "i1")
-          ->connect(*peak, "o1", "i1")
-          ->connect(*join, "o1", "i1")
-          ->connect(*o1, "o1", "i1");
-      i1->connect(*ma2)->connect(*diff, "o1", "i2");
-      i1->connect(*join, "o1", "i2");
+      // Build pipeline
+      i1->connect(ma1)->connect(diff, 0, 0)->connect(peak)->connect(join, 0, 0)->connect(o1);
+      i1->connect(ma2)->connect(diff, 0, 1);
+      i1->connect(join, 0, 1);
+      i1->connect(o1);
     }
   }
 
   void TearDown(const ::benchmark::State& state) {}
 
   int Process() {
-    uint passes = 100;
+    uint passes = 10000;
     // process the data
     for (auto j = 0u; j < passes; j++) {
       for (auto i = 0u; i < s->ti.size(); i++) {
-        i1->receiveData(Message<uint64_t, double>(s->ti[i], s->ppg[i]));
-        i1->executeData();
+        i1->receive_data(create_message<NumberData>(s->ti[i], NumberData{s->ppg[i]}), 0);
+        i1->execute();
       }
     }
 
     return passes * s->ti.size();
   }
 };
-
 
 BENCHMARK_DEFINE_F(RtBotFixture, Process)(benchmark::State& state) {
   int rows = 0;
@@ -75,8 +74,8 @@ BENCHMARK_DEFINE_F(RtBotFixture, Process)(benchmark::State& state) {
 
 // Register the function as a benchmark
 BENCHMARK_REGISTER_F(RtBotFixture, Process)
-    ->Name("Process sample size 7195 * 100 rows")
+    ->Name("Process sample size 7195 * 10000 rows")
     ->Unit(benchmark::kMillisecond)
-    ->Range(1, 1000);
+    ->Range(1, 10000);
 
 BENCHMARK_MAIN();
